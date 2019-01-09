d9baa47f778ef200c73e4e0c9befdc973458f798
Patrick Viet <patrick.viet@gmail.com>
2019-01-09 17:50:26 -0500

    policy stuff

--- api/lib/hdeploy/policy.rb
+++ api/lib/hdeploy/policy.rb
@@ -56,23 +56,72 @@ def validatepolicy20181218(policy)
   policy
 end
 
-def match_acl(matcher, tomatch)
-  matcher = [ matcher ] if matcher.is_a? String
+$policyfiles = [ 'policy.json', 'policy2.json' ]
+$policies = {}
 
+def load_policies(force = false)
+  # We will load and process policies alphabetically
+  newpolicies = {}
+  numloads = 0
+
+  begin
+    $policyfiles.sort.each do |file|
+      # Made on difference lines so it's easier to trace errors
+      mtime = File.stat(file).mtime.to_i
+      size = File.stat(file).size
+
+      if force or (not $policies.key?(file)) or ($policies.key?(file) and ($policies[file]['mtime'] != mtime or $policies[file]['size'] != size))
+        numloads += 1
+        raw = File.read(file)
+        policy = JSON.parse(raw)
+        newpolicy[file] = validatepolicy(policy)
+        newpolicy[file]['mtime'] = mtime
+        newpolicy[file]['size'] = size
+      else
+        puts "Skipping load of #{file}: mtime/size still the same"
+        newpolicies[file] = $policies[file]
+      end
+    end
+
+    # Happens at the end and only if no exception happened
+    puts "Finished loading - loaded #{numloads} files"
+    $policies = newpolicies
+  rescue Exception => e
+    raise "Could error while loading policies: #{e.backtrace} - keeping old policy settings"
+  end
+
+  $policies
 end
 
-def process_acl(action,resource,policy)
-  policy['Statement'].each do |statement|
-    sid,effect,p_action,p_resource = statement.values_at('Sid','Effect','Action','Resource')
+def process_acl(action,resource)
 
-    if (p_action.map{|a| File.fnmatch(a,action)}.include?(true)) and (p_resource.map{|r| File.fnmatch(r,resource)}.include?(true))
-      puts "Matched on sid #{sid}"
-      return (effect == "Allow")
+  final_result = nil
+
+  # load_acl gives an array of policies
+  load_policies().each do |file,policy|
+    require 'pry'
+    binding.pry
+    policy['Statement'].each do |statement|
+      sid,effect,p_action,p_resource = statement.values_at('Sid','Effect','Action','Resource')
+
+      if (p_action.map{|a| File.fnmatch(a,action)}.include?(true)) and (p_resource.map{|r| File.fnmatch(r,resource)}.include?(true))
+        puts "Matched on sid #{sid}"
+        final_result = (effect == "Allow")
+        break
+      end
+    end
+
+    if not final_result.nil?
       break
     end
   end
-  puts "No match - denying"
-  return false
+
+  if final_result.nil?
+    puts "No match for any policy - default is deny"
+    return false
+  else
+    return final_result
+  end
 end
 
-puts process_acl('DoStuff', 'blah:derp', validatepolicy(JSON.parse(File.read('policy.json'))))
+puts process_acl('DoStuff', 'blah:derp')
